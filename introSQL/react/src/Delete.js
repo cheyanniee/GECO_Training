b6895f142feef061c7bca69f9f75bb649baea5ef
Cheyanne <cheyannelimhui@gmail.com>
2022-10-13 20:15:33 +0800

    12/10/2022 Assignment

---
+++ introSQL/react/src/Delete.js
@@ -0,0 +1,67 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { useState, useEffect } from "react";
+
+function Delete(){
+    const [userID, setUserID] = useState();
+    const [emails, setEmails] = useState();
+
+    useEffect(() => {
+        fetch("http://localhost:8080/emails", {
+            method:"GET",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(res => res.json())
+        .then(res2 => {console.log(res2); setEmails(res2); console.log(emails);})
+        .catch(err => alert(err))
+    }, []);
+
+    const submit = event => {
+        console.log(userID);
+        fetch("http://localhost:8080/delete", {
+            method:"POST",
+            body:JSON.stringify({
+                "email": userID
+            }),
+            headers:{
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(res => res.json())
+        .then(res2 => {console.log(res2); alert(res2.message)})
+        .catch(err => alert(err))
+    }
+
+    return(
+        <body>
+            <Header/>
+
+            <h1>Delete User</h1>
+
+            <div className="formInputs">
+                <p>
+                    Target Email:
+                    <select onChange={(e)=>setUserID(e.target.value)}>
+                        <option value="amy@email.com"> amy@email.com</option>
+                        <option value="brenda@email.com"> brenda@email.com</option>
+                        <option value="charlie@email.com"> charlie@email.com</option>
+                        <option value="denise@email.com"> denise@email.com</option>
+                        <option value="elena@email.com"> elena@email.com</option>
+                        <option value="fiona@email.com"> fiona@email.com</option>
+                        <option value="groot@email.com"> groot@email.com</option>
+                        <option value="helen@email.com"> helen@email.com</option>
+                        <option value="iris@email.com"> iris@email.com</option>
+                    </select>
+                </p>
+
+                <button onClick={submit}>Delete</button>
+            </div>
+
+            <Footer/>
+        </body>
+)
+}
+
+export default Delete;
